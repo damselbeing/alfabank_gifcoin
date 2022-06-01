2a73ddd84d54bfee871b13fddd8d7b120c18e37c
damselbeing <damselbeing@gmail.com>
2022-06-01 19:53:34 +0300

    Project was created.

---
+++ src/main/java/alfabank/gifcoin/GifcoinApplication.java
@@ -0,0 +1,13 @@
+package alfabank.gifcoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifcoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GifcoinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/alfabank/gifcoin/GifcoinApplicationTests.java
@@ -0,0 +1,13 @@
+package alfabank.gifcoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GifcoinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
